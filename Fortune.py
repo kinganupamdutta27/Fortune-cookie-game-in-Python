2798aa3a63d030eb462845353c3fd012c98ce61f
Anupam Dutta <56832807+kinganupamdutta27@users.noreply.github.com>
2023-02-04 20:08:21 +0530

    Create Fortune.py
    import random
    
    fort1 = str("Today will be a lucky day")
    fort2 = str("You will meet someone new and interesting")
    fort3 = str("Watch out for obstacles in your path")
    
    x = random. randrange(1,3)
    if x==1:
      print(fort1)
    elif x==2:
      print(fort2)
    elif x==2:
      print(fort3)

---
+++ Fortune.py
@@ -0,0 +1,13 @@
+import random
+
+fort1 = str("Today will be a lucky day")
+fort2 = str("You will meet someone new and interesting")
+fort3 = str("Watch out for obstacles in your path")
+
+x = random. randrange(1,3)
+if x==1:
+  print(fort1)
+elif x==2:
+  print(fort2)
+elif x==2:
+  print(fort3)
